30d4d71da52e5357506c285f6d1be615f48c3280
MobbareKurtZ <hannes.skoog@gmail.com>
2018-04-19 14:33:06 +0200

    Absolute klar

--- dev/is_even.rb
+++
@@ -1,6 +0,0 @@
-def is_even(num)
-    if num % 2 == 1
-        return false
-    end
-    return true
-end
---
+++ lib/absolute.rb
@@ -0,0 +1,6 @@
+def absolute(num)
+    if num < 0
+        num = num * -1
+    end
+    return num
+end
