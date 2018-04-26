28d1a7716580386c7d386e039317f38a2c46e2a0
MobbareKurtZ <hannes.skoog@gmail.com>
2018-04-26 14:53:56 +0200

    Power klar

---
+++ lib/power.rb
@@ -0,0 +1,14 @@
+# Public: Takes two numbers and returns the power of num1 with num2 as exponent.
+#
+# num1  - Base number.
+# num2  - Exponent number.
+#
+# Examples
+#
+#   power(3,2)
+#   # => 9
+#
+# Returns power of num1 with num2 as exponent.
+def power(num1, num2)
+    return num1**num2
+end
