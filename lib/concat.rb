511e9a7960374c522a13e135e2e5625b48d2e1b8
MobbareKurtZ <hannes.skoog@gmail.com>
2018-05-03 13:39:50 +0200

    Concat klar

---
+++ lib/concat.rb
@@ -0,0 +1,14 @@
+# Public: Takes two arrays and concats them.
+#
+# arr1  - First array.
+# arr2  - Second array.
+#
+# Examples
+#
+#   concat([3,6,12,34], [3,6,3])
+#   # => [3,6,12,34,3,6,3]
+#
+# Returns concated array.
+def concat(arr1, arr2)
+    return arr1 + arr2
+end
