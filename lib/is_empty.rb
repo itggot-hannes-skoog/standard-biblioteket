94c955c294d79186ddd88ea1951fba90b38ec7f1
MobbareKurtZ <hannes.skoog@gmail.com>
2018-05-03 13:51:45 +0200

    IS empty klar

---
+++ lib/is_empty.rb
@@ -0,0 +1,13 @@
+# Public: Takes string and checks if empty.
+#
+# str  - Input string.
+#
+# Examples
+#
+#   is_empty("hej")
+#   # => false
+#
+# Returns if string is empty.
+def is_empty(str)
+   return str.size == 0 else false
+end
