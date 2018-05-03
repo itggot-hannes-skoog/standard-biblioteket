017c5a8da6e479b5b19d9ea1f0416500c35cfcad
MobbareKurtZ <hannes.skoog@gmail.com>
2018-05-03 13:34:41 +0200

    Last/first of klara

---
+++ lib/first_of.rb
@@ -0,0 +1,13 @@
+# Public: Takes an array and returns first value.
+#
+# arr  - Input array.
+#
+# Examples
+#
+#   first_of([3,6,12,34])
+#   # => 3
+#
+# Returns first number of array.
+def first_of(arr)
+    return arr[0]
+end
---
+++ lib/last_of.rb
@@ -0,0 +1,13 @@
+# Public: Takes an array and returns last value.
+#
+# arr  - Input array.
+#
+# Examples
+#
+#   last_of([3,6,12,34])
+#   # => 3
+#
+# Returns last number of array.
+def last_of(arr)
+    return arr[arr.size - 1]
+end
